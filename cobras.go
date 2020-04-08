ed766a2975ce8abc3c2e98a3cf47a813eba0f2b0
erwinvaneyk <erwinvaneyk@gmail.com>
2020-04-08 12:07:11 +0200

    Add initial code

---
+++ cobras.go
@@ -0,0 +1,62 @@
+package cobras
+
+import (
+	"context"
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"os/signal"
+)
+
+type Options interface {
+	Complete(cmd *cobra.Command, args []string) error
+	Validate() error
+	Run(ctx context.Context) error
+}
+
+func Run(opts Options) func(cmd *cobra.Command, args []string) {
+	return func(cmd *cobra.Command, args []string) {
+		err := opts.Complete(cmd, args)
+		if err != nil {
+			printErrorAndDie(err)
+		}
+
+		err = opts.Validate()
+		if err != nil {
+			printErrorAndDie(err)
+		}
+
+		ctx, cancel := Context()
+		defer cancel()
+
+		err = opts.Run(ctx)
+		if err != nil {
+			printErrorAndDie(err)
+		}
+	}
+}
+
+func Context() (ctx context.Context, cancel func()) {
+	// trap Ctrl+C and call cancel on the context
+	ctx, origCancel := context.WithCancel(context.Background())
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		select {
+		case <-c:
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+	return ctx, func() {
+		func() {
+			signal.Stop(c)
+			origCancel()
+		}()
+	}
+}
+
+func printErrorAndDie(err error) {
+	fmt.Fprintf(os.Stderr, "error: %v", err)
+	os.Exit(1)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/erwinvaneyk/cobras
+
+go 1.14
+
+require github.com/spf13/cobra v0.0.7
